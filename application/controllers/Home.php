172e7f9f2db18653d0d296a43c141e2a9cd784b1
Daniel Blay <danielblay0@gmail.com>
2022-02-17 02:43:47 +0000

    college management system (php)

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Home extends CI_Controller {
+class Home extends MY_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -23,6 +23,25 @@ class Home extends CI_Controller {
 		$this->load->view('Home');
 	}
 	public function adminRegister(){
-		$this->load->view('register');
+	    $this->load->model('queries');
+	    $roles = $this->queries->getRoles();
+		$this->load->view('register', ['roles'=>$roles]);
 	}
+    public function adminSignup(){
+        $this->form_validation->set_rules('username', 'Username', 'required');
+        $this->form_validation->set_rules('email', 'Email', 'required');
+        $this->form_validation->set_rules('gender', 'Gender', 'required');
+        $this->form_validation->set_rules('role_id', 'Role', 'required');
+        $this->form_validation->set_rules('password', 'Password', 'required');
+        $this->form_validation->set_rules('Confirm Password', 'Confirm Password', 'required');
+
+        $this->form_validation->set_error_delimiters('<div class="text-danger">','</div>'
+            );
+        if( $this->form_validation->run() ){
+            echo 'Validation passed';
+        }
+        else {
+            echo validation_errors();
+        }
+    }
 }
--- application/views/register.php
+++ application/views/register.php
@@ -1,5 +1,103 @@
 <?php include("inc/header.php");?>
 <div class="container">
-  <h1>ADMIN REGISTRATION<h1>
+    <?php echo form_open("home/adminSignup", ['class'=> 'form-horizontal']);?>
+    <h3>ADMIN REGISTRATION<h3>
+    <hr>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="form-group", style=color:black>
+          <label class="col-md-3 control-label">Username</label>
+          <div>
+            <?php echo form_input(['name'=> 'username', 'class'=>'form-control', 'placeholder'=>'Username']);?>
+          </div>
+        </div>
+       </div>
+       <div class="col-md-6">
+           <?php echo form_error('username','<div class="text-danger">','</div>');
+           ?>
+       </div>
+     </div>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="form-group", style=color:black>
+          <label class="col-md-3 control-label">Email</label>
+          <div>
+            <?php echo form_input(['name'=> 'email', 'class'=>'form-control', 'placeholder'=>'Email']);?>
+          </div>
+        </div>
+       </div>
+       <div class="col-md-6">
+           <?php echo form_error('email','<div class="text-danger">','</div>');
+           ?>
+       </div>
+     </div>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="form-group", style=color:black>
+          <label class="col-md-3 control-label">Gender</label>
+          <select class="col-lg-9" name="gender">
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+          </select>
+          </div>
+        </div>
+       </div>
+       <div class="col-md-6">
+           <?php echo form_error('gender','<div class="text-danger">','</div>');
+           ?>
+       </div>
+     </div>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="form-group">
+          <label class="col-md-3 control-label">Role</label>
+          <select class="col-lg-9" name="role-id">
+              <option value="">Select</option>
+          <?php if(count($roles)):?>
+              <?php foreach($roles as $role):?>
+              <option value=<?php echo $role->rolename?>><?php echo $role->
+                  rolename?></option>
+              <?php endforeach;?>
+          <?php endif;?>
+          </select>
+          </div>
+        </div>
+       </div>
+       <div class="col-md-6">
+           <?php echo form_error('role_id','<div class="text-danger">','</div>');
+           ?>
+       </div>
+     </div>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="form-group">
+          <label class="col-md-3 control-label">Password</label>
+          <div class="col-md-9">
+            <?php echo form_input(['name'=> 'password', 'class'=>'form-control', 'placeholder'=>'password']);?>
+          </div>
+        </div>
+       </div>
+       <div class="col-md-6">
+           <?php echo form_error('password','<div class="text-danger">','</div>');
+           ?>
+       </div>
+     </div>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="form-group">
+          <label class="col-md-3 control-label">Confirm Password</label>
+          <div class="col-md-9">
+            <?php echo form_input(['name'=> 'Confirm Password', 'class'=>'form-control', 'placeholder'=>'password']);?>
+          </div>
+        </div>
+       </div>
+       <div class="col-md-6">
+           <?php echo form_error('Confirm Password','<div class="text-danger">','</div>');
+           ?>
+       </div>
+     </div>
+  <button type="submit" class="btn btn-primary">REGISTER</button>
+  <?php echo anchor("Home", "BACK", ['class'=>'btn btn-primary']);?></div>
+  <?php echo form_close();?>
 </div>
 <?php include("inc/footer.php");?>
